eb7298476884ae45abacbe4dcf83ad0debb81b24
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-18 23:18:46 +0530

    split class(sentiment) and tweet

--- client.py
+++ client.py
@@ -1,3 +1,7 @@
+'''
+Om Arham Mukha Kamal Vaasinee Paapaatma Kshayam Kaari Vad Vad Vaagwaadinee Saraswati Aing Hreeng Namah Swaaha 
+'''
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql import SQLContext
@@ -8,14 +12,16 @@ sc = SparkContext("local[2]", "NetworkWordCount")
 ssc = StreamingContext(sc, 1)
 
 lines = ssc.socketTextStream("localhost", 6100)
-
+#print(lines)
 # Count each word in each batch
-counts = lines.flatMap(lambda line: line.split(" "))\
-                  .map(lambda word: (word, 1))\
-                  .reduceByKey(lambda a, b: a+b)
+records = lines.flatMap(lambda line: line.split('\\n",')).map(lambda x: x.split(','))
+#records = lines.foreachRDD(lambda line: line)
+records.pprint(20)
+#for record in records:
+#	print(record)
 
 # Print the first ten elements of each RDD generated in this DStream to the console
-counts.pprint()
+#counts.pprint()
 
 ssc.start()
 ssc.awaitTermination()

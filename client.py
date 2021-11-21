fced77c5e71aa5100002c2c4054f6cb4a8ead091
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-21 23:03:13 +0530

    need to update schema

--- client.py
+++ client.py
@@ -3,26 +3,64 @@ Om Arham Mukha Kamal Vaasinee Paapaatma Kshayam Kaari Vad Vad Vaagwaadinee Saras
 '''
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SQLContext, Row, SparkSession
 import sys
 
 # config
 sc = SparkContext("local[2]", "NetworkWordCount")
 ssc = StreamingContext(sc, 1)
 
-mt = sc.emptyRDD()
-
 def clean(x):
 	x = x.replace('\\n', '')
 	x = x.replace('\\', '')
-	print(x)
 	return x
 
 lines = ssc.socketTextStream("localhost", 6100)
 a = lines.map(lambda x: clean(x)).reduce(lambda x, y : x.join(y))
-#records = lines.flatMap(lambda line: line.split('\\n"')).map(lambda x: x.split(','))
-a.pprint(50)
+
+def getSparkSessionInstance(sparkConf):
+    if ('sparkSessionSingletonInstance' not in globals()):
+        globals()['sparkSessionSingletonInstance'] = SparkSession\
+            .builder\
+            .config(conf=sparkConf)\
+            .getOrCreate()
+    return globals()['sparkSessionSingletonInstance']
+
+def process(time, rdd):
+    print("========= %s =========" % str(time))
+
+    # Get the singleton instance of SparkSession
+    spark = getSparkSessionInstance(rdd.context.getConf())
+
+    # Convert RDD[String] to RDD[Row] to DataFrame
+    rowRdd = rdd.map(lambda w: Row(word=w))
+    wordsDataFrame = spark.createDataFrame(rowRdd)
+
+    # Creates a temporary view using the DataFrame.
+    wordsDataFrame.createOrReplaceTempView("words")
+
+    # Do word count on table using SQL and print it
+    wordCountsDataFrame = \
+        spark.sql("select word, count(*) as total from words group by word")
+    wordCountsDataFrame.show()
+            
+lines = ssc.socketTextStream('localhost', 6100)
+words = lines.map(lambda line: line)
+words.pprint()
+words.foreachRDD(process)
 ssc.start()
 ssc.awaitTermination()
-print(a)
+'''mt = sc.emptyRDD()
+
+def clean(x):
+	x = x.replace('\\n', '')
+	x = x.replace('\\', '')
+	return x
+
+lines = ssc.socketTextStream("localhost", 6100)
+a = lines.map(lambda x: clean(x)).reduce(lambda x, y : x.join(y))
+
+a.pprint(50) '''
+
+
 

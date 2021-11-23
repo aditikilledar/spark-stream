27998e945cad688904c99b640ce1c56a24fb7198
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-11-23 22:38:19 +0530

    preproc works, need to split into sentiment and tweet

--- client.py
+++ client.py
@@ -2,65 +2,49 @@
 Om Arham Mukha Kamal Vaasinee Paapaatma Kshayam Kaari Vad Vad Vaagwaadinee Saraswati Aing Hreeng Namah Swaaha 
 '''
 from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.sql import SQLContext, Row, SparkSession
+from pyspark.streaming import StreamingContext, DStream
+from pyspark.sql import SQLContext
 import sys
+from typing import Tuple
+
 
 # config
 sc = SparkContext("local[2]", "NetworkWordCount")
 ssc = StreamingContext(sc, 1)
+sqc = SQLContext(sc)
+
+def line_to_tuple(line: str) -> Tuple[str, str]:
+        try:
+            k, v = line.split(",")
+            return k, v
+        except ValueError:
+            return "", ""
 
 def clean(x):
 	x = x.replace('\\n', '')
 	x = x.replace('\\', '')
 	return x
 
-lines = ssc.socketTextStream("localhost", 6100)
-a = lines.map(lambda x: clean(x)).reduce(lambda x, y : x.join(y))
+def test(x):
+	x.toDF().show()
 
-def getSparkSessionInstance(sparkConf):
-    if ('sparkSessionSingletonInstance' not in globals()):
-        globals()['sparkSessionSingletonInstance'] = SparkSession\
-            .builder\
-            .config(conf=sparkConf)\
-            .getOrCreate()
-    return globals()['sparkSessionSingletonInstance']
-
-def process(time, rdd):
-    print("========= %s =========" % str(time))
-
-    # Get the singleton instance of SparkSession
-    spark = getSparkSessionInstance(rdd.context.getConf())
-
-    # Convert RDD[String] to RDD[Row] to DataFrame
-    rowRdd = rdd.map(lambda w: Row(word=w))
-    wordsDataFrame = spark.createDataFrame(rowRdd)
-
-    # Creates a temporary view using the DataFrame.
-    wordsDataFrame.createOrReplaceTempView("words")
-
-    # Do word count on table using SQL and print it
-    wordCountsDataFrame = \
-        spark.sql("select word, count(*) as total from words group by word")
-    wordCountsDataFrame.show()
-            
 lines = ssc.socketTextStream('localhost', 6100)
-words = lines.map(lambda line: line)
-words.pprint()
-words.foreachRDD(process)
+col_path = "./columns.csv"
+line_tuple = ssc.sparkContext.textFile(col_path).map(line_to_tuple)
+PLS = lines.flatMap(lambda line: line.split(','))
+lines.transform(lambda rdd: test(rdd))
+#lines.flatMap(lambda x: x.split(',')).foreachRDD(lambda x: test(x))
+ltc = line_tuple.collect()
+#PLS.pprint()
+for i in ltc:
+	print(i)
+#line_tuple.toDF().show()
+#rdd2 = line_tuple.flatMapValues(lambda x : [ (k, x[k]) for k in x.keys()])
+#line_tuple.pprint()
+#words = lines.map(lambda line: clean(line)).reduce(lambda x, y: x.join(y))
+#words.transform(lambda x: x.take(20))
+#words.pprint(20)
+
 ssc.start()
 ssc.awaitTermination()
-'''mt = sc.emptyRDD()
-
-def clean(x):
-	x = x.replace('\\n', '')
-	x = x.replace('\\', '')
-	return x
-
-lines = ssc.socketTextStream("localhost", 6100)
-a = lines.map(lambda x: clean(x)).reduce(lambda x, y : x.join(y))
-
-a.pprint(50) '''
-
-
 

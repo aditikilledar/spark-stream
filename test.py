353fc763e958ff0ecf9316556c30eefaa76bb164
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-12-06 01:32:04 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,96 @@
+'''
+Om Arham Mukha Kamal Vaasinee Paapaatma Kshayam Kaari Vad Vad Vaagwaadinee Saraswati Aing Hreeng Namah Swaaha 
+'''
+import pickle
+import sys
+import json
+import re
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext, DStream
+from pyspark.sql import SQLContext, Row, SparkSession
+from pyspark.sql.functions import lit
+from pyspark.ml.classification import NaiveBayes, DecisionTreeClassifier, LogisticRegression, GBTClassifier, LinearSVC, RandomForestClassifier
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.cluster import MiniBatchKMeans
+from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.util import MLUtils
+from pyspark.ml.feature import StopWordsRemover, Word2Vec, RegexTokenizer, CountVectorizer, HashingTF
+import nltk
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+import numpy as np
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+ 
+lemmatizer = WordNetLemmatizer()
+stwords = stopwords.words('english')
+
+morestwords = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+stwords += morestwords
+
+# config
+sc = SparkContext("local[2]", "NetworkWordCount")
+spark = SparkSession(sc)
+ssc = StreamingContext(sc, 1)
+sqc = SQLContext(sc)
+
+global vectorizer
+vectorizer = HashingTF(inputCol='Tweet', outputCol='features')
+vectorizer.setNumFeatures(10)
+
+global sknb
+global skbnb
+global sksgd
+global sknb_model
+global skbnb_model
+global sksgd_model
+sknb_model = pickle.load(open('NaiveBayes.sav','rb'))
+skbnb_model = pickle.load(open('BernoulliBayes.sav','rb'))
+sksgd = SGDClassifier()
+
+def preproc(item):
+	#removing punctuation, @, RT, making it lower case
+	item = re.sub('http\S+', '', item)
+	item = re.sub('@\w+', '', item)
+	item = re.sub('#', '', item)
+	item = re.sub('RT', '', item)
+	item = re.sub(':', '', item)
+	item = re.sub('",', '', item)
+	item = re.sub('\\n', '', item)
+	item = re.sub(r'[^\w\s]', ' ', item)
+	item = item.lower()
+	item = re.sub(r'\d+', '', item)
+	item = [word for word in item.split(' ') if word not in stwords]
+	item = [lemmatizer.lemmatize(word) for word in item if word != '']
+	nitem = ''
+	for word in item:
+		nitem += word + ' '
+	
+	return nitem
+
+def get_pred(tweet):
+	print('hi')
+	if not tweet.isEmpty():
+		df = spark.createDataFrame(tweet)
+		label_list = df.select('label').collect()
+		Y = [row.label for row in label_list]
+		result = vectorizer.transform(df)
+		feature_list = result.select('features').collect()
+		X = [row.features.toArray() for row in feature_list]
+		print('NaiveBayes: ',sknb_model.score(X, Y))
+		print('BernoulliBayes: ',skbnb_model.score(X, Y))
+		
+
+lines = ssc.socketTextStream('localhost', 6100)
+lines = lines.flatMap(lambda line: json.loads(line))
+lines = lines.filter(lambda line: line[0] != 'S')
+tweets = lines.map(lambda tweet: Row(label=float(tweet[0]),Tweet=preproc(tweet[2:]).split(' ')))
+tweets.foreachRDD(get_pred)
+ssc.start()
+ssc.awaitTermination()

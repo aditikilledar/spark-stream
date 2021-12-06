f34b9dc6637bd89c0c17f63ba98e31323636f7ed
gauthamys <66833388+gauthamys@users.noreply.github.com>
2021-12-06 23:04:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ def get_pred(tweet):
 
 		print('----------Bernoulli Naive Bayes----------')
 		#print('BernoulliBayes: ',skbnb_model.score(X, Y))
-		nb_report = classification_report(Y, sknb_model.predict(X), labels=np.unique(Y))
+		sknb_report = classification_report(Y, sknb_model.predict(X), labels=np.unique(Y))
 		#skbnb_conf = confusion_matrix(Y, sknb_model.predict(X), labels=np.unique(Y))
 		#print('Confusion_Matrix: ', skbnb_conf)
 		print('Report: ', skbnb_report)
